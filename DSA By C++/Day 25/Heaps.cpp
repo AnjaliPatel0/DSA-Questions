351083507e18027a701c2c9e739dec7e49833676
AnjaliPatel0 <anjalipatela78@gmail.com>
2026-03-31 20:08:50 +0530

    delete heap

--- DSA By C++/Day 25/Heaps.cpp
+++ DSA By C++/Day 25/Heaps.cpp
@@ -32,6 +32,33 @@ class heap{
         cout<<arr[i]<<" ";
     }cout<<endl;
   }
+
+  void deletefromHeap(){
+    if(size==0){
+        cout<<"nothing to delete"<<endl;
+        return;
+    }
+    //put last element in first root element
+    arr[1]=arr[size];
+    size--;
+
+    //take root node to its correct position
+    int i=1;
+    while(i<size){
+        int leftIndex=2*i;
+        int rightIndex=2*i+1;
+
+        if(leftIndex<size && arr[i]<arr[leftIndex]){
+            swap(arr[i],arr[leftIndex]);
+            i=leftIndex;
+        }else if(rightIndex<size && arr[i]<arr[rightIndex]){
+            swap(arr[i],arr[rightIndex]);
+            i=rightIndex;
+        }else{
+            return ;
+        }
+    }
+  }
 };
 int main(){
    heap h;
@@ -40,5 +67,8 @@ int main(){
    h.insert(53);
    h.insert(52);
    h.insert(54);
+   
+   h.print();
+   h.deletefromHeap();
    h.print();
 }
